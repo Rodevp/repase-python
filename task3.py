2219181ed85c83ab93dc2c4509e119f1a46e7ed7
Rodevp <rodriguezrodrigor12@gmail.com>
2022-08-10 17:37:04 -0500

    task 3

---
+++ task3.py
@@ -0,0 +1,11 @@
+name = 'rodrigo'
+last_name = 'rodriguez'
+age = 21
+email = 'example@gmail.com'
+is_merry = False
+with_child = False
+friends = ['juan', 'jeni']
+fav_movies = {
+    'title': 'Django',
+    'author': 'Tarantino'
+}
