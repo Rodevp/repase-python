453592eee9f6a34428eadafcc5f12ae30b653613
Rodevp <rodriguezrodrigor12@gmail.com>
2022-08-10 20:08:26 -0500

    task 4.2

--- task4.py
+++ task4.py
@@ -5,16 +5,14 @@ if age >= 18 :
 else :
     print('no eres mayor de edad')
 
-
 start = int( input( 'inicio: ' ) )
 end = int( input( 'final: ' ) )
 
 for n in range(start, end) :
     print(n)
 
-
 n_100 = 100
 
 while n_100 > 0 :
-    print(n)
+    print(n_100)
     n_100 = n_100 - 1
