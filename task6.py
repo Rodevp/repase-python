edddfa3105cb99fcc9629e903f2aadd61f27542c
Rodevp <rodriguezrodrigor12@gmail.com>
2022-08-14 10:21:16 -0500

    task 6.1

--- task6.py
+++ task6.py
@@ -17,3 +17,23 @@ class Coche(Vehiculo) :
 c = Coche('rojo', 4, 4, 100, 4)
 print(c)
 
+
+#tarea 2
+
+class Alumno :
+
+    def __init__(self, nombre, nota = 0):
+        self.nombre = nombre
+        self.nota = nota
+
+    def asignarNota(self, n) :
+        self.nota = n
+
+    def obtenerNota(self) :
+        return self.nota
+
+    def aprobado(self) :
+        return "aprobado" if self.nota >= 3 else "no aprobado" 
+
+    def nombreAlumno(self) :
+        return self.nombre
