f96bd64365e50fdfaea80fed799ab204347593fe
Rodevp <rodriguezrodrigor12@gmail.com>
2022-08-17 19:41:47 -0500

    task 7

---
+++ op.py
@@ -0,0 +1,11 @@
+def suma(a, b) :
+    print(a + b)
+
+def resta(a, b) :
+    print(a - b)
+
+def mul(a, b) :
+    print(a * b)
+
+def div(a, b) :
+    print(a / b)
---
+++ task7.py
@@ -0,0 +1,6 @@
+from op import suma, resta, div, mul
+
+suma(1,3)
+resta(2,3)
+div(4,2)
+mul(3,3)
