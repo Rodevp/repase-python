c51f60ad85a11d97e10146e8f10cac5270e02f9a
Rodevp <rodriguezrodrigor12@gmail.com>
2022-08-17 19:52:03 -0500

    task 7.1

--- task7.py
+++ task7.py
@@ -1,6 +1,20 @@
+from datetime import datetime
+
 from op import suma, resta, div, mul
 
 suma(1,3)
 resta(2,3)
 div(4,2)
 mul(3,3)
+
+
+now = datetime.now()
+current_time =  now.strftime("%H:%M:%S")
+
+if not current_time.split(':')[0] == '19' :
+    print()
+    current = int( current_time.split(':')[0] )
+    r = current - 19
+    print(f'te faltan #{r} para salir de trabajar')
+else :
+    print('ya no estás trabajando')
